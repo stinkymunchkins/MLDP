f0e80c13829e380edeb4afae9a6c0ffdb023a205
stinkymunchkins <2401521F@student.tp.edu.sg>
2025-07-30 14:11:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ with open("styles.css") as f:
 st.set_page_config(page_title="HDB Resale Price Predictor", page_icon="🏠", layout="wide")
 
 
-model = joblib.load('randomforestmodel.pkl')
+model = joblib.load('rf_model.joblib')
 
 # --- custom banner with gradient background ---
 st.markdown("""
